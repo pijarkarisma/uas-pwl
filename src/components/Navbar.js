e07176243160a31533f233cc7810f46ed7456453
Error512 <keivan.roin09@gmail.com>
2023-01-12 12:04:10 +0700

    presentasi

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ export default class Navbar extends Component {
                 <ul className="navbar-nav align-items-center">
                     <li className="nav-item ml-5">
                         <Link to="/" className="nav-link">
-                            products
+                            Course
                         </Link>
                     </li>
                 </ul>
